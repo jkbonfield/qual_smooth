0e706683fc75bb12ac92e9ffcba953ff130b0958
James Bonfield <jkb@sanger.ac.uk>
2022-11-04 12:31:18 +0000

    Plot.pl now pipes into gnuplot itself, plus multiplot.
    We add all 4 kmer graphs into a single new kmer4.png plot.

--- plot.pl
+++ plot.pl
@@ -10,9 +10,12 @@ my $fn = shift(@ARGV);
 my $title = $fn;
 $title =~ s#.*/##;
 $title =~ s/\.stat$//;
+$title =~ s/_/-/g;
 
-# Kmer plot
-print << "EOF"
+#-----------------------------------------------------------------------------
+# Kmer plot; overlapping
+open(GP, "|gnuplot");
+print GP << "EOF"
 set terminal "pngcairo"
 set output "$fn.kmer.png"
 set title "KMERS: $title" font "Helvetica,12"
@@ -28,7 +31,62 @@ plot x lt 0 notitle, \\
 "<grep QUAL   $fn | grep -v -- -2147" using 5:4 with linespoints lw 2 lc 4 title "Qual"
 EOF
 ;
+close(GP);
 
+#-----------------------------------------------------------------------------
+# Kmer multi-plot
+open(GP, "|gnuplot");
+print GP << "EOFm"
+set terminal pngcairo size 800,600
+set output "$fn.kmer4.png"
+
+yl="Predicted Phred score"
+xl="Measured Phred score"
+set yrange [0:\`awk '/(ALL|UNDER|MATCH)2/ {v=v<\$8?\$8:v} END {print 5*(int((v+9.99)/5))}' $fn\`]
+set xrange [0:\`awk '/(ALL|UNDER|MATCH)2/ {v=v<\$6?\$6:v} END {print 5*(int((v+5.99)/5))}' $fn\`]
+set xtics offset 0,0.3
+
+#set xrange [0:30]
+#set yrange [0:25]
+
+a=rand(-1)
+
+set grid
+set multiplot
+unset title
+
+# Top left: mismatch
+set label "KMERS: $title" font "Helvetica,12" at graph 1.1,1.2 centre
+unset xlabel
+set ylabel yl offset 1.5
+set size 0.53,0.46
+set origin 0,0.46
+plot "<grep MATCH2 $fn" using (\$6+rand(0)):(\$8+rand(0)):2 with labels hypertext point lt 7 ps 0.2 lc 1 title "Mismatch"
+unset ylabel
+unset label
+
+# Top right: overcalls
+set size 0.5,0.46
+set origin 0.5,0.46
+plot "<grep OVER2  $fn" using (\$6+rand(0)):(\$8+rand(0)):2 with labels hypertext point lt 7 ps 0.2 lc 8 title "Overcall"
+
+# Bottom left: undercalls
+set size 0.53,0.48
+set xlabel xl offset 0,0.8
+set ylabel yl offset 1.5
+set origin 0,0.01
+plot "<grep UNDER2 $fn" using (\$6+rand(0)):(\$8+rand(0)):2 with labels hypertext point lt 7 ps 0.2 lc 20 title "Undercall"
+unset ylabel
+
+# Bottom right: Combined
+set size 0.5,0.48
+set origin 0.5,0.01
+plot "<grep ALL2   $fn" using (\$6+rand(0)):(\$8+rand(0)):2 with labels hypertext point lt 7 ps 0.2 lc 6 title "Combined"
+EOFm
+;
+close(GP);
+
+#-----------------------------------------------------------------------------
 # ACGT* -> ACGT* heatmap
 # Figures are a percentage of the total base count.
 foreach my $sec (qw/SUBST_HQ SUBST_ALL/) {
@@ -67,18 +125,21 @@ foreach my $sec (qw/SUBST_HQ SUBST_ALL/) {
 	}
 	$mat .= "\n";
     }
-    #print STDERR "$mat\n";
     close($fh);
 
     my $subst = lc($sec);
     my $subst2 = $subst; $subst2 =~ s/_/ /g;
-    print << "EOF2"
+
+    open(GP, "|gnuplot");
+    print GP << "EOF2"
 \$mat << EOD
 $mat
 EOD
 set terminal "pngcairo"
 set output "$fn.$subst.png"
 set title "$subst2 error \%age matrix: $title" offset 0,0.5 font "Helvetica,12"
+set palette rgb 34,35,36
+#set palette rgb 7,3,23
 set xlabel "call"
 set ylabel "cons" offset -2
 set size 0.95,1
@@ -89,4 +150,5 @@ set yrange [-0.5:4.5]
 splot '\$mat' matrix rowheaders columnheaders using 1:2:3 with image notitle
 EOF2
 ;
+    close(GP);
 }
